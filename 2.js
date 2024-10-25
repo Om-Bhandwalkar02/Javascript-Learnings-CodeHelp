0221c737622f6b9cd6570d77510a6f96043e458b
Om Bhandwalkar <155551098+Om-Bhandwalkar02@users.noreply.github.com>
2024-10-25 22:27:33 +0530

    for loop

--- 2.js
+++ 2.js
@@ -1,1 +1,45 @@
 // Loops
+
+//counting 0 to 5
+for(i=0; i<=5; i++){
+    console.log(i);
+}
+
+console.log('----------------------------------------------')
+console.log('----------------------------------------------')
+
+// reverse counting 5 to 0
+
+for (i = 5; i>=0; i--){
+    console.log(i);
+}
+
+
+console.log('----------------------------------------------')
+console.log('----------------------------------------------')
+
+
+for(i = 1; i<=10; i++){
+    console.log(i)
+    if (i===4){
+        break
+    }
+}
+
+console.log('----------------------------------------------')
+console.log('----------------------------------------------')
+
+
+for(i = 1; i<=10; i++){
+    console.log(i)
+    if (i===4){
+    continue
+    }
+}
+// break --> 'aree ruk jaa bhai...'
+// continue --> 'ye waala skip kar re ... fatafat  aage chal'
+
+console.log('----------------------------------------------')
+console.log('----------------------------------------------')
+
+
