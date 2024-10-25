d070deb9e6e913b4298dbcc0ff828c67eef85637
Om Bhandwalkar <155551098+Om-Bhandwalkar02@users.noreply.github.com>
2024-10-25 23:20:13 +0530

    loops and string

--- 2.js
+++ 2.js
@@ -1,7 +1,10 @@
 // Loops
 
+
+//For Loop
+
 //counting 0 to 5
-for(i=0; i<=5; i++){
+for(let i=0; i<=5; i++){
     console.log(i);
 }
 
@@ -10,7 +13,7 @@ console.log('----------------------------------------------')
 
 // reverse counting 5 to 0
 
-for (i = 5; i>=0; i--){
+for (let i = 5; i>=0; i--){
     console.log(i);
 }
 
@@ -19,7 +22,7 @@ console.log('----------------------------------------------')
 console.log('----------------------------------------------')
 
 
-for(i = 1; i<=10; i++){
+for(let i = 1; i<=10; i++){
     console.log(i)
     if (i===4){
         break
@@ -30,7 +33,7 @@ console.log('----------------------------------------------')
 console.log('----------------------------------------------')
 
 
-for(i = 1; i<=10; i++){
+for(let i = 1; i<=10; i++){
     console.log(i)
     if (i===4){
     continue
@@ -43,3 +46,65 @@ console.log('----------------------------------------------')
 console.log('----------------------------------------------')
 
 
+//While Loop
+
+let i = 1
+while (i<=20) {
+
+    console.log(i);
+    
+    i++;
+}
+
+console.log('----------------------------------------------')
+console.log('----------------------------------------------')
+
+
+// do while Loop
+//yaha pe pehle kaam hoga/ iteration hogi fir condition check hogi
+
+let j = 1;
+do{
+    console.log(j)
+    j++;
+}while(j<=10)
+
+console.log('----------------------------------------------')
+console.log('----------------------------------------------')
+
+
+// Strings
+//Sequence characters
+
+let firstName = 'Om'
+let lastName = 'Bhandwalkar'
+
+// Template string ``
+
+let fullName = `Om
+Bhandwalkar
+`
+
+console.log(firstName + ' ' + lastName)
+console.log(fullName)
+
+console.log(`${firstName} ${lastName}`)
+
+console.log(firstName.toUpperCase())
+
+
+//Substring
+// ek string ka part
+
+let str = 'Danish bhai zinda hote toh.......'
+console.log(str.substring(2, 4))
+// start index included a ... end index excluded a
+
+
+let words = str.split(' '); //words ek array bangaya
+
+console.log(words)
+
+
+// to convert array to string
+console.log(words.join('-'))
