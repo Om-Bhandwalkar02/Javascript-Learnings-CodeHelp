128938447a17c36b24c2682fdfdd3fcfda636342
Om Bhandwalkar <155551098+Om-Bhandwalkar02@users.noreply.github.com>
2024-10-27 03:31:45 +0530

    Temporal Dead Zone

---
+++ 6.js
@@ -0,0 +1,32 @@
+// variable scoping
+//  --> Global
+//  --> Function
+//  --> Block
+
+
+// global scope
+var age = 5
+// let, var, const these all 3 keywords work here
+
+
+
+console.log(num);
+
+{
+    var num = 2;
+}
+
+console.log(num);
+// variable created by var keyword is not a block scope variale
+
+
+
+
+
+//Temporal Dead zone
+// yaha se shuru hua   .....hum marks value is zone mein access nahi kar sakte
+console.log(marks)
+console.log('xihesdjfcle')
+console.log('fcerhdvrefgvn')
+const marks = 100 //yaha pe khatam hua
+console.log(marks)
